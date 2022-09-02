f80d38541d11f0ab34f207d9e8a483404e9fb195
nicey <laisi1127@163.com>
2022-09-02 09:56:55 +0800

    重新布局构思，增加主页和新增页，主页显示明细，新增页记账

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,86 +1,34 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <Layout>
-    <nav>
-      <ul class="types">
-        <li class="selected">支出</li>
-        <li>收入</li>
-      </ul>
-    </nav>
-    <Expenditures></Expenditures>
-    <Revenue></Revenue>
-    <label class="notes">
-      <span class="name">备注</span>
-      <input type="text" />
-    </label>
-    <div class="numberPad">
-      <div class="output">100</div>
-      <div class="buttons">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>×</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>-</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>+</button>
-        <button>再记</button>
-        <button>0</button>
-        <button>.</button>
-        <button>保存</button>
+  <div>
+    <Layout>
+      <p>图片</p>
+      <div class="new-wrapper">
+        <router-link to="/new">
+          <Icon name="new" />
+        </router-link>
       </div>
-    </div>
-  </Layout>
+      <div>money.vue</div>
+      <div>支出或者收入明细树</div>
+    </Layout>
+  </div>
 </template>
-
 <script lang="ts">
-import Expenditures from "@/components/Expenditures.vue";
-import Revenue from "../components/Revenue.vue";
-export default {
-  // eslint-disable-next-line vue/no-unused-components
-  components: { Expenditures, Revenue },
-};
+export default {};
 </script>
 
 <style lang="scss" scoped>
 @import "@/assets/style/helper.scss";
+
 .new-wrapper {
   overflow: auto;
+  border: 1px solid red;
   .icon {
     width: 4em;
     height: 4em;
     color: $color-highlight;
     fill: currentColor;
     overflow: hidden;
-  }
-}
-nav {
-  > .types {
-    background: $color-background;
-    display: flex;
-    text-align: center;
-    font-size: 24px;
-    > li {
-      width: 50%;
-      height: 64px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      position: relative;
-      &.selected::after {
-        content: "";
-        position: absolute;
-        bottom: 0;
-        left: 25%;
-        width: 50%;
-        height: 3px;
-        background: rgb(109, 107, 107);
-      }
-    }
   }
 }
 </style>
---
+++ src/views/New.vue
@@ -0,0 +1,86 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <Layout>
+    <nav>
+      <ul class="types">
+        <li class="selected">支出</li>
+        <li>收入</li>
+      </ul>
+    </nav>
+    <Expenditures></Expenditures>
+    <Revenue></Revenue>
+    <label class="notes">
+      <span class="name">备注</span>
+      <input type="text" />
+    </label>
+    <div class="numberPad">
+      <div class="output">100</div>
+      <div class="buttons">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>×</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>-</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>+</button>
+        <button>再记</button>
+        <button>0</button>
+        <button>.</button>
+        <button>保存</button>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<script lang="ts">
+import Expenditures from "@/components/Expenditures.vue";
+import Revenue from "../components/Revenue.vue";
+export default {
+  // eslint-disable-next-line vue/no-unused-components
+  components: { Expenditures, Revenue },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/style/helper.scss";
+.new-wrapper {
+  overflow: auto;
+  .icon {
+    width: 4em;
+    height: 4em;
+    color: $color-highlight;
+    fill: currentColor;
+    overflow: hidden;
+  }
+}
+nav {
+  > .types {
+    background: $color-background;
+    display: flex;
+    text-align: center;
+    font-size: 24px;
+    > li {
+      width: 50%;
+      height: 64px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+      &.selected::after {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        left: 25%;
+        width: 50%;
+        height: 3px;
+        background: rgb(109, 107, 107);
+      }
+    }
+  }
+}
+</style>
